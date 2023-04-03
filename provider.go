d9ac3f99a9df6a13c5f9a160ae0747e719f898d1
MasterEvarior <contaxt@giannin.dev>
2023-04-03 16:51:43 +0200

    Trailing dot is now removed from the zone.

--- provider.go
+++ provider.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/libdns/libdns"
 )
@@ -23,9 +24,9 @@ const apiHost = "https://api.ns1.hosttech.eu/api/user/v1"
 
 // GetRecords lists all the records in the zone.
 func (p *Provider) GetRecords(ctx context.Context, zone string) ([]libdns.Record, error) {
-	reqURL := fmt.Sprintf("%s/zones/%s/records", apiHost, zone)
+	reqURL := fmt.Sprintf("%s/zones/%s/records", apiHost, cleanZone(zone))
 
-	responseBody, err := p.makeApiCall(ctx, http.MethodGet, reqURL, nil, zone)
+	responseBody, err := p.makeApiCall(ctx, http.MethodGet, reqURL, nil)
 
 	//If there's an error return an empty slice
 	if err != nil {
@@ -50,7 +51,7 @@ func (p *Provider) GetRecords(ctx context.Context, zone string) ([]libdns.Record
 // AppendRecords adds records to the zone. It returns all records that were added.
 // If an error occurs while records are being added, the already successfully added records will be returned along with an error.
 func (p *Provider) AppendRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
-	reqURL := fmt.Sprintf("%s/zones/%s/records", apiHost, zone)
+	reqURL := fmt.Sprintf("%s/zones/%s/records", apiHost, cleanZone(zone))
 
 	successfullyAppendedRecords := []libdns.Record{}
 	for _, record := range records {
@@ -65,7 +66,7 @@ func (p *Provider) AppendRecords(ctx context.Context, zone string, records []lib
 			return nil, err
 		}
 
-		responseBody, err := p.makeApiCall(ctx, http.MethodPost, reqURL, bytes.NewReader(bodyBytes), zone)
+		responseBody, err := p.makeApiCall(ctx, http.MethodPost, reqURL, bytes.NewReader(bodyBytes))
 		if err != nil {
 			return successfullyAppendedRecords, err
 		}
@@ -99,9 +100,9 @@ func (p *Provider) SetRecords(ctx context.Context, zone string, records []libdns
 			return nil, err
 		}
 
-		reqURL := fmt.Sprintf("%s/zones/%s/records/%s", apiHost, zone, record.ID)
+		reqURL := fmt.Sprintf("%s/zones/%s/records/%s", apiHost, cleanZone(zone), record.ID)
 
-		responseBody, err := p.makeApiCall(ctx, http.MethodPut, reqURL, bytes.NewReader(bodyBytes), zone)
+		responseBody, err := p.makeApiCall(ctx, http.MethodPut, reqURL, bytes.NewReader(bodyBytes))
 
 		if err != nil {
 			//If the error doesn't have anything to do with the api, return
@@ -142,8 +143,8 @@ func (p *Provider) SetRecords(ctx context.Context, zone string, records []libdns
 func (p *Provider) DeleteRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
 	successfullyDeletedRecords := []libdns.Record{}
 	for _, record := range records {
-		reqUrl := fmt.Sprintf("%s/zones/%s/records/%s", apiHost, zone, record.ID)
-		_, err := p.makeApiCall(ctx, http.MethodDelete, reqUrl, nil, zone)
+		reqUrl := fmt.Sprintf("%s/zones/%s/records/%s", apiHost, cleanZone(zone), record.ID)
+		_, err := p.makeApiCall(ctx, http.MethodDelete, reqUrl, nil)
 
 		if err != nil {
 			return successfullyDeletedRecords, err
@@ -155,7 +156,7 @@ func (p *Provider) DeleteRecords(ctx context.Context, zone string, records []lib
 	return successfullyDeletedRecords, nil
 }
 
-func (p *Provider) makeApiCall(ctx context.Context, httpMethod string, reqUrl string, body io.Reader, zone string) (response []byte, err error) {
+func (p *Provider) makeApiCall(ctx context.Context, httpMethod string, reqUrl string, body io.Reader) (response []byte, err error) {
 	req, err := http.NewRequestWithContext(ctx, httpMethod, reqUrl, body)
 	req.Header.Set("Authorization", "Bearer "+p.APIToken)
 	req.Header.Set("Content-Type", "application/json")
@@ -180,19 +181,11 @@ func (p *Provider) makeApiCall(ctx context.Context, httpMethod string, reqUrl st
 	}
 
 	return io.ReadAll(resp.Body)
+}
 
-	/*
-		err = json.NewDecoder(resp.Body).Decode(response)
-
-		if err != nil {
-			return []libdns.Record{}, err
-		}
-
-		var libdnsRecords []libdns.Record
-		for _, record := range parsedResponse.Data {
-			libdnsRecords = append(libdnsRecords, record.toLibdnsRecord(zone))
-		}
-	*/
+// Removes the trailing dot from the zone
+func cleanZone(zone string) string {
+	return strings.TrimRight(zone, ".")
 }
 
 // Interface guards
